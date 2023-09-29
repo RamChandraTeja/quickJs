5e3f7885a2d34b64f71801d2c6b6096540dce5fe
rama <krct.112@gmail.com>
2023-09-28 23:53:04 -0400

    Delete quickJs directory

--- quickJs/session1.js
+++
@@ -1,4 +0,0 @@
-const currentTime = new Date()
-const oneHourAhead = currentTime.getTime()+(60*60*1000);
-const lateDate =new Date(oneHourAhead).toTimeString()
-console.log({currentTime:currentTime.toTimeString()},{lateDate});
